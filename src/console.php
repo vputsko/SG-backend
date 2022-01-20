911726f34c7ec129af5f2e62cd44d701120cbe2e
vputsko <vpu.muk@gmail.com>
2022-01-20 19:40:17 +0100

    delete tests command

--- src/console.php
+++ src/console.php
@@ -8,16 +8,15 @@ $app = new Silly\Application();
 
 $app->useContainer($container, true, true);
 
-$app->command('get_user [user_id]', 'App\Command\GetUserCommand');
-$app->command('consume [receivers] [-l|--limit=] [-f|--failure-limit=] [-m|--memory-limit=] [-t|--time-limit=] [-b|--bus] [--sleep] [--queues] [--no-reset]', 'App\Command\SymfonyConsumeMessagesCommand')->defaults([
+$app->command('consume [receivers] [-l|--limit=] [-f|--failure-limit=] [-m|--memory-limit=] [-t|--time-limit=] [-b|--bus] [--sleep] [--queues] [--no-reset]', ['App\Command\SymfonyConsumeMessagesCommand', 'execute'])->defaults([
     'receivers' => ['async'],
     'limit' => 1,
     'failure-limit' => 0,
     'memory-limit' => 0,
     'time-limit' => 1,
 ]);
-$app->command('send_money [limit]', 'App\Command\SendMoneyCommand')->defaults([
-    'limit' => 0
+$app->command('send_money [limit]', ['App\Command\SendMoneyCommand', 'execute'])->defaults([
+    'limit' => 0,
 ]);
 
 $app->setDefaultCommand('consume');
