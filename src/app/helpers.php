ba694222d2415716f9a438f8e9d0855785650d4d
vputsko <vpu.muk@gmail.com>
2022-01-20 19:37:03 +0100

    add the container getter

--- src/app/helpers.php
+++ src/app/helpers.php
@@ -5,10 +5,23 @@ declare(strict_types = 1);
 namespace App;
 
 use Laminas\Diactoros\Response\TextResponse;
+use DI\Container;
 use Psr\Http\Message\ResponseInterface;
 use function function_exists;
 
-if (! \function_exists('response')) {
+if (! function_exists('container')) {
+    /**
+     * Return the container
+     * 
+     * @return Container
+     */
+    function container(): Container
+    {
+        return require __DIR__ . "/../bootstrap/app.php";
+    }
+}
+
+if (! function_exists('response')) {
     /**
      * Return a new response
      */
